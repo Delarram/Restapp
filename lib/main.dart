d51932e5cf14fbbcd553a5256f5d1765f53b37a0
Delarram <Delurram@gmail.com>
2022-07-28 15:32:22 +0630

    added home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rest_api/screens/home_screen.dart';
 import 'package:rest_api/views/home_page.dart';
 
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const HomeScreen(),
     );
   }
 }
