9286cbaa40d99de19d041c21b841cba68f433efc
Delarram <tunyannaing960@gmail.com>
2022-09-09 01:14:11 +0630

    check error and fix

--- lib/pagination/refresh_listview_itemview.dart
+++ lib/pagination/refresh_listview_itemview.dart
@@ -1,11 +1,44 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
-class RefreshListViewItemView extends StatelessWidget {
+class RefreshListViewItemView extends StatefulWidget {
    RefreshListViewItemView({Key? key}) : super(key: key);
-   final List<String> items = ["car","train","flight","shop","car","train","flight","shop","car","train","flight","shop","car","train","flight","shop",];
+
+  @override
+  State<RefreshListViewItemView> createState() => _RefreshListViewItemViewState();
+}
+
+class _RefreshListViewItemViewState extends State<RefreshListViewItemView> {
+  final List<String> items = ["car","train","flight","shop","car","train","flight","shop","car","train","flight","shop","car","train","flight","shop",];
+
+  RefreshController _refreshController =
+   RefreshController(initialRefresh: false);
+
+  void _onRefresh() async{
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
+  }
+
+  void _onLoading() async{
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use loadFailed(),if no data return,use LoadNodata()
+    items.add((items.length+1).toString());
+    if(mounted)
+      setState(() {
+      });
+    _refreshController.loadComplete();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return   ListView.builder(
+    return items.isEmpty? Center(child: CircularProgressIndicator()):
+      ListView.builder(
+        physics: NeverScrollableScrollPhysics(),
+      itemExtent: 100.0,
       shrinkWrap: true,
       itemCount: items.length,
       itemBuilder: (context, index) {
