b411a6619809b3cbfd00668f363c3772bbe9e4ba
Delarram <tunyannaing960@gmail.com>
2022-09-12 13:44:11 +0630

    added refresh indicator and zoom page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rest_api/pagination/refresh_listview.dart';
+import 'package:rest_api/pagination/zoom_text.dart';
 import 'package:rest_api/screens/landingScreen/landing_screen.dart';
 import 'package:rest_api/screens/welcomeScreen/forclipper/language_welcome_screen.dart';
 
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: RefreshListView()
+        home: ZoomText()
       );},
     );
   }
