6ff6315bec93d038f76a79d88d48d9fdefda479a
Delarram <tunyannaing960@gmail.com>
2022-09-22 10:14:41 +0630

    facebook login

--- lib/facebook_signin/facebook_sign_in.dart
+++ lib/facebook_signin/facebook_sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 class FaceBookSignIn extends StatefulWidget {
   const FaceBookSignIn({Key? key}) : super(key: key);
@@ -8,18 +9,35 @@ class FaceBookSignIn extends StatefulWidget {
 }
 
 class _FaceBookSignInState extends State<FaceBookSignIn> {
+  Map? _userData;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("FaceBook SignIn"),
+        title: Text('Facebook (Logged ' + (_userData == null ? 'Out' : 'In') + ')'),
         elevation: 3,
       ),
       body: SafeArea(
         child:Column(
           children: [
                 MaterialButton(color: Colors.blue,
-                  onPressed: (){},child: Text("Log in"),
+                  onPressed: () async{
+                    final result = await FacebookAuth.i.login(
+                        permissions: ["public_profile", "email"]
+                    );
+
+                    if (result.status == LoginStatus.success) {
+
+                      final userData = await FacebookAuth.i.getUserData(
+                        fields: "email,name",
+                      );
+
+                      setState(() {
+                        _userData = userData;
+                      });
+
+                    }
+                  },child: Text("Log in"),
                 ),
             MaterialButton(
               color: Colors.blue,
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:rest_api/facebook_signin/facebook_sign_in.dart';
