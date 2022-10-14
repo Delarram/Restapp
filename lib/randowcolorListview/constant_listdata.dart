bfd7692ee391564d199b7a890010a60b86719598
Delarram <tunyannaing960@gmail.com>
2022-10-14 13:36:23 +0630

    color listview

---
+++ lib/randowcolorListview/constant_listdata.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class DataList {
+  static List<Color> colorList = [
+    Color(0xffFFFFFF),
+    Color(0xff5A73D7),
+    Color(0xffF56F37),
+    Color(0xffDA192C),
+    Color(0xffA5C40F),
+  ];
+}
