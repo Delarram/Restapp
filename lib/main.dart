9bb6643f5920d06925754fb2ba1993f000d8162a
Delarram <tunyannaing960@gmail.com>
2022-11-09 21:10:50 +0630

    refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rest_api/cupertino_picker/cupertino_picker.dart';
 import 'package:rest_api/recentview/course_page.dart';
+import 'package:rest_api/videoplayer/video_player_page.dart';
 
 
 Future<void> main() async {
@@ -40,7 +41,7 @@ class MyApp extends StatelessWidget {
             theme: ThemeData(
               primarySwatch: Colors.blue,
             ),
-            home:CoursePage()
+            home:VideoPlayerScreen()
         );
       },
     );
--- lib/videoplayer/video_player_page.dart
+++ lib/videoplayer/video_player_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rest_api/videoplayer/video.dart';
 
 import 'package:video_player/video_player.dart';
@@ -14,21 +15,37 @@ class VideoPlayerScreen extends StatefulWidget {
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
 
   late VideoPlayerController _controller;
+  int _currentIndex = 0 ;
+
+
+  void _playVideo({int index = 0 , bool  init = false}){
+     if(index< 0 || index >= videos.length) return;
+
+     _controller = VideoPlayerController.network(videos[_currentIndex].url)
+     ..addListener(()=>setState((){}))
+     ..setLooping(true)
+     ..initialize().then((value) => _controller.play());
+  }
+
 
   @override
   void initState() {
+ _playVideo();
     super.initState();
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.landscapeRight,
       DeviceOrientation.landscapeLeft,
     ]);
   }
+
+
   @override
   dispose() {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.landscapeRight,
       DeviceOrientation.landscapeLeft,
     ]);
+    _controller.dispose();
     super.dispose();
   }
 
@@ -41,6 +58,16 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
             height: 300,
             width: double.infinity,
             color: Colors.purple,
+            child:_controller.value.isInitialized ?
+            Column(
+              children: [
+               SizedBox(
+                 height: 200.h,
+                 child:VideoPlayer(_controller),
+               )
+              ],
+            ) :
+            Center(child: CircularProgressIndicator(color: Colors.white)),
           ),
           Flexible(
             child: ListView.builder(
@@ -49,14 +76,14 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
                 itemBuilder: (context, index)=>
                     Padding(
                       padding: const EdgeInsets.symmetric(
-                          horizontal: 15,vertical: 10),
+                          horizontal: 15,vertical:10),
                       child: Row(
                         children: [
                           Container(
                               color: Colors.pink,
                               height:100,
                               width:100,
-                              margin: const EdgeInsets.all(8),
+                              margin:const EdgeInsets.all(8),
                               child: Image.network(videos[index].thumbnail,fit: BoxFit.contain,)),
                           const SizedBox(width: 8,height: 8,),
                           Text("${videos[index].name}")
